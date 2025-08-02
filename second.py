6944f223f3d6ef85f240351f6d0b5ffe59fa600f
Praful Thakur <rajputpraful791@gmail.com>
2025-08-02 14:55:33 +0530

    Add files via upload

---
+++ second.py
@@ -0,0 +1,34 @@
+import pandas as pd
+# data = {
+#     "name": ["John","ram",None],
+#     "age": [23,None,23],
+#     "city": ["New York","Los Angeles","Chicago"]
+# }
+# df = pd.DataFrame(data)
+# # print(df)
+# print(df.isnull())
+# print(df.notnull())
+# print(df.isnull().sum())
+# print(df.notnull().sum())
+# print(df.isnull().sum(axis=1))
+# print(df.notnull().sum(axis=1))
+# print(df.isnull().sum(axis=0))
+# print(df.notnull().sum(axis=0))
+# print(df.fillna(27))
+# df['age'] = df['age'].fillna(27)
+# df['name'] = df['name'].fillna("Tushar")
+# print(df)
+# print(df.dropna())
+# print(df.dropna(axis=1))
+print("heerllo_baji")
+data={"name": ["John","ram","sita","tushar","Delta"],
+      "City": ["New York","Los Angeles","Chicago","Delhi","Mumbai"],
+      "Age": [23,27,25,30,22.33]}
+# hello=pd.DataFrame(data)
+# # print(hello)
+# avg_age=hello.groupby('City')['Age'].mean()
+# print(avg_age)
+# f_avg_age=avg_age.round()
+# print(f_avg_age)
+
+# print(type(hello))
