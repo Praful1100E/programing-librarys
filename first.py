ac43405281e34bb0a822df772f611541289aa260
Praful Thakur <rajputpraful791@gmail.com>
2025-08-03 21:56:00 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,23 @@
+import pandas as pd
+# data =[1,2,3,4,5,]
+# series = pd.Series(data)
+# print(series)
+
+
+# data = {"Name": ["Alice", "Bob", "Charlie"],
+#         "Age": [25, 30, 35],}
+# baji = pd.DataFrame(data)
+# print(baji)
+
+# x=pd.Series("Praful",index=(1,2,3,4,5))
+# print(x)
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+print("Hello")
+print(myvar)
+
